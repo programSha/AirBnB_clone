9ef7f42f7ba673abe4d74cf0cefa5ac60f3644df
programSha <sharonkirwa19@gmail.com>
2023-12-20 11:03:15 +0000

    DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage_type
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
                               cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        name = ''
+        state_id = ''
